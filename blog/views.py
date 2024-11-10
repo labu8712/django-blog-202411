6198a41e8c981a25b7092330f0a94c4008e95af9
Arthur Chang <arthurc0102@gmail.com>
2024-11-10 15:53:17 +0800

    Add post create form

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 
+from blog.forms import PostForm
 from blog.models import Post
 
 
@@ -9,4 +11,10 @@ def post_list(request):
 
 
 def post_create(request):
-    return render(request, "blog/post_create.html")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Post create success.")
+        return redirect("blog:post-list")
+
+    return render(request, "blog/post_create.html", {"form": form})
