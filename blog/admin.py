2c4e3fa053fdd479552b9266f7d33be586807c39
Arthur Chang <arthurc0102@gmail.com>
2024-11-10 14:22:47 +0800

    Add admin for blog models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Category, Comment, Post, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "category")
+    list_filter = ("category", "tags", "status")
+    search_fields = ("title", "content")
+    filter_horizontal = ("tags",)
+    date_hierarchy = "created_at"
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("nick_name", "post")
+    list_filter = ("post",)
+    search_fields = ("nick_name", "content")
+    date_hierarchy = "created_at"
