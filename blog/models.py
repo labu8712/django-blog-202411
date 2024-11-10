85579a227112badffbd6fcd6924f829d7edd04ae
Arthur Chang <arthurc0102@gmail.com>
2024-11-10 13:56:45 +0800

    Create blog app

---
+++ blog/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        PUBLIC = "public", "Public"
+        PRIVATE = "private", "Private"
+
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    status = models.CharField(
+        max_length=7,
+        choices=Status,
+        default=Status.PUBLIC,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Comment(models.Model):
+    nick_name = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.content
