ff54cb621267d2207af4ffef05420dbb543e9cf0
Arthur Chang <arthurc0102@gmail.com>
2024-12-01 14:19:31 +0800

    Add custom validation for form

--- blog/forms.py
+++ blog/forms.py
@@ -15,3 +15,20 @@ class PostForm(forms.ModelForm):
         widgets = {
             "tags": forms.CheckboxSelectMultiple,
         }
+
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        if not title.startswith("ABC"):
+            raise forms.ValidationError("你的文章標題需要以 ABC 開頭")
+
+        return title + " end"
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        title = cleaned_data.get("title", "")
+        content = cleaned_data.get("content", "")
+        if (len(title) + len(content)) <= 10:
+            raise forms.ValidationError("文章太短")
+
+        return cleaned_data
