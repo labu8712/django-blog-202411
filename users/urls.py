472d9043bc19e8786d8758996fc596cd10cfa74a
Arthur Chang <arthurc0102@gmail.com>
2024-12-08 15:40:26 +0800

    Add password change and password change done

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import views as django_auth_views
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from users import views as user_views
 
@@ -16,4 +16,19 @@ urlpatterns = [
     ),
     path("logout/", django_auth_views.LogoutView.as_view(), name="logout"),
     path("register/", user_views.register, name="register"),
+    path(
+        "password/change/",
+        django_auth_views.PasswordChangeView.as_view(
+            template_name="users/password_change.html",
+            success_url=reverse_lazy("users:password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "password/change/done",
+        django_auth_views.PasswordChangeDoneView.as_view(
+            template_name="users/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
 ]
