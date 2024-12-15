3717204e77be4adc5e03d83f9d25bfde9888d5db
Arthur Chang <arthurc0102@gmail.com>
2024-12-15 11:43:34 +0800

    Add category description

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
 
     def __str__(self) -> str:
         return self.name
