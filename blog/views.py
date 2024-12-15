6c51ed272d66068fcaac9cc39a40bf34e75f2f95
Arthur Chang <arthurc0102@gmail.com>
2024-12-15 14:22:29 +0800

    Show image in frontend

--- blog/views.py
+++ blog/views.py
@@ -40,7 +40,7 @@ def post_detail(request, pk):
 @login_required
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    form = PostForm(request.POST or None, instance=post)
+    form = PostForm(request.POST or None, request.FILES or None, instance=post)
     if form.is_valid():
         form.save()
         messages.success(request, "Post update success.")
